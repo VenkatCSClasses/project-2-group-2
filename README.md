a7e0fdc9d3259994286f8872b2755da839e36bb9
untitledham <abemanfra@gmail.com>
2026-04-03 16:04:48 -0400

    Login, log out routes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # IC Food Thing
+
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/login")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    return {"access_token": "<token will go here>", "token_type": "bearer"}
+
+
+@app.get("/logout")
+async def logout():
+    return {"message": "Logged out"}
