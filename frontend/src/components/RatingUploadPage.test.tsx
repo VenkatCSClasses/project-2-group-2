61c0f40c226c4e0be2fb2225fc517765f0edcd4e
harrison-spangler <harrison.spangler@gmail.com>
2026-04-06 14:36:44 -0400

    feat: make rating form controlled

--- frontend/src/components/RatingUploadPage.test.tsx
+++ frontend/src/components/RatingUploadPage.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { test, expect } from 'vitest'
 import RatingUploadPage from './RatingUploadPage'
 
@@ -13,4 +14,27 @@ test('renders upload rating form', () => {
   expect(screen.getByLabelText(/description/i)).toBeInTheDocument()
   expect(screen.getByLabelText(/image/i)).toBeInTheDocument()
   expect(screen.getByRole('button', { name: /submit/i })).toBeInTheDocument()
+})
+
+test('allows the user to type into form fields', async () => {
+  const user = userEvent.setup()
+  render(<RatingUploadPage />)
+
+  const itemIdInput = screen.getByLabelText(/item id/i)
+  const itemNameInput = screen.getByLabelText(/item name/i)
+  const diningHallInput = screen.getByLabelText(/dining hall/i)
+  const ratingInput = screen.getByLabelText(/rating/i)
+  const descriptionInput = screen.getByLabelText(/description/i)
+
+  await user.type(itemIdInput, '123')
+  await user.type(itemNameInput, 'Pizza')
+  await user.type(diningHallInput, 'RPCC')
+  await user.type(ratingInput, '5')
+  await user.type(descriptionInput, 'Pretty good')
+
+  expect(itemIdInput).toHaveValue('123')
+  expect(itemNameInput).toHaveValue('Pizza')
+  expect(diningHallInput).toHaveValue('RPCC')
+  expect(ratingInput).toHaveValue(5)
+  expect(descriptionInput).toHaveValue('Pretty good')
 })
